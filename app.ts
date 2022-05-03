3fe303046c688647bb3f595a92ae2388ebc1664a
Andro Bošnjak <bosnjak.andro1@gmail.com>
2022-05-03 14:45:46 +0200

    feat: add pawn routes and documentation

--- app.ts
+++ app.ts
@@ -1,5 +1,19 @@
 import express, { Application } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import swaggerUi from 'swagger-ui-express';
+import pawnRoutes from './routes/pawn';
+import * as swaggerDocument from './swagger.json';
 
 const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(morgan('tiny'));
+app.use(helmet());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use('/pawn', pawnRoutes);
 
 app.listen(3000, () => console.log('API is running on port 3000'));
